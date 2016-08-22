81d1a053681b4347ab61613809c66ea5c37191cb
Max Krieger <themaxaxis@gmail.com>
2016-08-22 12:51:50 -0400

    updated things

--- components/SocialMedia/SocialMedia.js
+++ components/SocialMedia/SocialMedia.js
@@ -17,7 +17,7 @@ class SocialMedia extends Component {
     return (
       <div className="SocialMedia row center-xs end-sm">
 				<a href="https://twitter.com/a9_io"><i className="fa fa-twitter"></i></a>
-				<a href="https://www.linkedin.com/in/max-k-10a62a106"><i className="fa fa-linkedin"></i></a>
+				<a href="https://www.linkedin.com/in/max-krieger"><i className="fa fa-linkedin"></i></a>
 				<a href="https://medium.com/@a9"><i className="fa fa-medium"></i></a>
 				<a href="https://www.last.fm/user/a9_io"><i className="fa fa-lastfm"></i></a>
 				<a href="https://github.com/maxkrieger"><i className="fa fa-github"></i></a>
--- pages/index.js
+++ pages/index.js
@@ -42,6 +42,10 @@ export default class extends Component {
 					<ProjectBox clicked={(item) => this.showProject(item)} title="alienbox" image="alienbox.png"/>
 					<ProjectBox clicked={(item) => this.showProject(item)} title="Tabmaster" image="tabmaster.png"/>
 				</Grid>
+        <Grid title="Experience">
+          <ProjectBox clicked={(item) => this.showProject(item)} title="CabinetCloud" image="cabinet.png" />
+          <ProjectBox clicked={(item) => this.showProject(item)} title="MIT Racecar" image="racecar.png" />
+        </Grid>
         <Section title="About">
           <div className="row">
           <div className="col-sm-6 col-xs-12">
@@ -49,7 +53,6 @@ export default class extends Component {
           </div>
           <div className="col-sm-6 col-xs-12">
             <p>I also participate in my school's robotics team and run the Linguistics club there.</p>
-            <p>Right now I intern for <a href="http://www.cabinetcrm.com/">CabinetCloud</a> where I design interfaces and develop our mobile app.</p>
           </div>
           </div>
         </Section>
@@ -75,6 +78,42 @@ export default class extends Component {
             </div>
           </div>
 				</Modal>
+        <Modal show={(this.state.show === 'CabinetCloud')} back={() => this.hideProject()} title="CabinetCloud">
+            <div className="col-sm-6 col-xs-12">
+                <p>I remotely interned at a local Boston-based startup called CabinetCloud with a team of 5 to develop a CRM product for local governments. The product would leverage smart government for local municipalities, aiding in their communication, organization, and analyzing with constituents. It received a microgrant from the Santander Urban Impact program, and had the endorsement of multiple local legislators.</p>
+                <p>My main work was to design user workflows and layouts for the web-based product. I kept the users, mainly local legislators in mind with my designs. A few months in, I was tasked with developing a full mobile implementation of the product.</p>
+                <p>To accomplish the mobile implementation, I sought for new technologies which would allow me to rapidly work on a cross-platform app solo. I came across a relatively new technology, called React Native, which allowed me to make UI elements composable and reusable, keeping design patterns consistent and understandable.</p>
+                <p>Unfortunately, the product's development is currently on-hold, but images of my work are available.</p>
+            </div>
+            <div className="col-sm-6 col-xs-12">
+              <div className="row center-xs">
+                <div>
+                  <img width="500" src="cabinet.png" />
+                </div>
+              </div>
+            </div>
+        </Modal>
+        <Modal show={(this.state.show === 'MIT Racecar')} back={() => this.hideProject()} title="MIT Beaverworks Racecar">
+          <div className="col-sm-6 col-xs-12">
+            <p>In the summer of 2016, I participated in MIT/Lincoln Laboratories' first institute for high schoolers. We were tasked with an engineering project - automating a remote control car with multiple sensors and an Nvidia Jetson GPU.</p>
+            <p>I had the privilege of seeing countless lecturers visit to talk about working in the field of mobile robotics and automation. In addition, MIT professors and colleagues of Lincoln Laboratories conducted seminars on developing control systems, localization, and computer vision for mobile robots</p>
+            <p>I collaborated with <a href={'http://august.codes/'}>August Trollbäck</a> on writing a report following the program.</p>
+            <div className="row center-xs">
+              <a href="http://a9.io/bwsi-report/">
+                <div className="visitButton" style={{backgroundColor: '#0351B9'}}>
+                  View Report
+                </div>
+              </a>
+            </div>
+          </div>
+          <div className="col-sm-6 col-xs-12">
+            <div className="row center-xs">
+              <div>
+                <img width="500" src="racecar.png" />
+              </div>
+            </div>
+          </div>
+        </Modal>
         <Modal show={(this.state.show === 'Tabmaster')} back={() => this.hideProject()} title="Tabmaster">
           <div className="col-sm-6 col-xs-12">
             <p>Tabmaster is a chrome extension allowing for easier management of stray tabs.</p>
