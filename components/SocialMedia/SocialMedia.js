6db418c03279f2575322c62cc3a8eb99848b1760
Max Krieger <themaxaxis@gmail.com>
2018-02-17 22:34:22 -0500

    disabled lastfm

--- components/SocialMedia/SocialMedia.js
+++ components/SocialMedia/SocialMedia.js
@@ -18,7 +18,7 @@ class SocialMedia extends Component {
       <div className="SocialMedia row center-xs end-sm">
 				<a href="https://twitter.com/a9_io"><i className="fa fa-twitter"></i></a>
 				<a href="https://www.linkedin.com/in/max-krieger"><i className="fa fa-linkedin"></i></a>
-				<a href="https://www.last.fm/user/a9_io"><i className="fa fa-lastfm"></i></a>
+				{/*<a href="https://www.last.fm/user/a9_io"><i className="fa fa-lastfm"></i></a>*/}
 				<a href="https://github.com/maxkrieger"><i className="fa fa-github"></i></a>
 				<a href="https://www.google.com/recaptcha/mailhide/d?k=01OQgZinQEdZ8NZXVTw0niRQ==&amp;c=aAHOFQgEVekfGiM4ENP0F1tGDqNTi4RZzNjnYcjE33U="><i className="fa fa-envelope"></i></a>
       </div>
